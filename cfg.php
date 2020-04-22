97e9aa46d8e51a061d3cab303c80f1ff76460e50
ihwan321 <63368199+ihwan321@users.noreply.github.com>
2020-04-23 01:36:18 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,22 @@
+<?php
+
+/* https://www.earnyourbtc.online/?r=EC-UserId-59035 */
+$agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36";
+$PHPSESSID ="PHPSESSID=2804215ef38f23c541d10e120c807e7f";
+$coke ="ppu_main_3cdb3545c6b9fe7a69be4a67a7c2872c=1";
+$cuki ="ppu_sub_3cdb3545c6b9fe7a69be4a67a7c2872c=4";
+$DOGEToken ="DOGEToken=G1LjSHsd3hHTk6ZduVd6RIRjsGJLUleU";
+
+/* https://getdogecoins.com/?r=EC-UserId-59035 */
+$gen = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36";
+$sess ="PHPSESSID=nv05g8a7e4ijmk10jqtn5likoi; DOGEToken=P8NthtEmu1M2McTq0j3Dmik6y3pUuRTJ;";
+
+/* https://westbit.online/?r=EC-UserId-59035 */
+$agent1 = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36";
+$PHPSESSID1 ="PHPSESSID=2bb523ab15871a799a07d8cc0893bd1a";
+$LTCToken ="LTCToken=9BFG14BV3b0siToupcoePn2ONcUoQF5J";
+
+/* https://faucetdogecoins.com/?r=EC-UserId-59035 */
+$agent4 = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36";
+$PHPSESSID4 ="PHPSESSID=9e033198eb4c6e74349769c4b4856077";
+$DOGEToken4 ="DOGEToken=8IwTWd2QvPIXpaSZBSna16KV9OMW89N7";
